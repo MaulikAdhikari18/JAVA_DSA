0b400fe18b2341471f9dc725f5f60879b2679989
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-01-21 15:56:37 +0530

    search insert Position

--- src/Arrays.java
+++ src/Arrays.java
@@ -20,4 +20,17 @@ public class Arrays {
             System.out.println(marks[i]);
         }
     }
+
+    //Search Insert Position
+    public int searchInsert(int[] nums, int target) {
+        for(int i = 0;i < nums.length;i++){
+            if(nums[i] == target)return i;
+        }
+        if(target < nums[0])return 0;
+        if(target > nums[nums.length-1])return nums.length;
+        for(int i = 0;i < nums.length-1;i++){
+            if(target>nums[i] && target<nums[i+1])return i+1;
+        }
+        return 0;
+    }
 }
