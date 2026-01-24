f20028015a783a5869e907dbe95d9c28d01a32f0
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-01-24 11:23:59 +0530

    Power Function

--- src/Functions.java
+++ src/Functions.java
@@ -36,7 +36,27 @@ public class Functions {
         System.out.println("Average of the three numbers:" + average);
         return 0;
     }
-    
+
+    //Power function
+    public double myPow(double x, int n) {
+        if(n == 0)return 1;
+        if(x == 0)return 0;
+         long N = n;
+         if(N < 0){
+            x = 1/x;
+            N = -N;
+         }
+         double result = 1.0;
+
+         while(N > 0){
+            if ((N & 1) == 1) {
+            result *= x;
+        }
+        x *= x;
+        N /= 2;
+    }
+         return result;
+    }
 
     
     public static void main(String[] args) {
