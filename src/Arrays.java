4206025207358784535c229e71c256c876e758e8
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-01-24 14:33:21 +0530

    Remove Elements

--- src/Arrays.java
+++ src/Arrays.java
@@ -61,4 +61,16 @@ public class Arrays {
          }
          return count;
     }
+
+    //remove element
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        for(int i = 0;i < nums.length;i++){
+            if(nums[i] != val){
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
 }
