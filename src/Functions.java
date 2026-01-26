bb3646708c1dff1c56b00882c2e3e08c7518b471
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-01-26 14:34:27 +0530

    Calculate Square Root

--- src/Functions.java
+++ src/Functions.java
@@ -73,6 +73,28 @@ public class Functions {
         return -1;
     }
 
+    //Calculate Square Root
+    public int mySqrt(int x) {
+        if(x < 2)return x;
+
+        int start = 1;
+        int end = x;
+        int mid = -1;
+
+        while(start <= end){
+            mid = start + (end - start) / 2;
+
+            if((long) mid * mid > (long)x)
+            end = mid - 1;
+            else if(mid * mid == x)
+            return mid;
+            else
+            start = mid + 1;
+        }            
+
+        return Math.round(end);    
+    }
+
     
     public static void main(String[] args) {
         Scanner sc  = new Scanner(System.in);
