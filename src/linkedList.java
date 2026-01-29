af3194f37b7cc1952b4f14195e01c8ebc67a458e
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-01-29 22:41:52 +0530

    Remove Duplicates II

--- src/linkedList.java
+++ src/linkedList.java
@@ -128,6 +128,26 @@ class lList{
         }
         return dummy.next;
 }
+
+//Remove Duplicates II
+public Node deleteDuplicates(Node head) {
+        Node dummy = new Node(0);
+        dummy.next = head;
+        Node prev = dummy;
+        Node curr = head;
+        while(curr != null){
+            while(curr.next != null && curr.data == curr.next.data){
+                curr = curr.next;
+            }
+            if(prev.next == curr){
+                prev = prev.next;
+            }else{
+                prev.next = curr.next;
+            }
+            curr = curr.next;
+        }
+        return dummy.next;
+    }
 }
 
 
