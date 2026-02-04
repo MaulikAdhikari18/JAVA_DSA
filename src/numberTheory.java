173ae3f82f8867f3885e1320df6000b3935f0cda
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-02-04 11:13:14 +0530

    Binary Ones

--- src/numberTheory.java
+++ src/numberTheory.java
@@ -14,4 +14,31 @@ public class numberTheory{
         return addDigits(sum);
         return sum;
     }
+
+    //Count Primes
+    public static int countPrimes(int n) {
+        if(n == 0 || n == 1)return 0;
+        if(n%2 == 0)return 0 + countPrimes(n-1);
+        for(int i = 2;i < n;i++){
+            if(n%i == 0)return 0 + countPrimes(n-1);
+        }
+        return 1 + countPrimes(n-1);
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(countPrimes(n));
+    }
+
+    //Number of ones in binary form
+    public int hammingWeight(int n) {
+        int count = 0;
+
+    while (n != 0) {
+        count += (n & 1);
+        n >>>= 1;   
+    }
+
+    return count;   
+    }
 }
