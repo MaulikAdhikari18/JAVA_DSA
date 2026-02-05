abeea01fece473bb8583e9e21cfe09329a9565be
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-02-05 19:32:43 +0530

    GCD of an Array

--- src/numberTheory.java
+++ src/numberTheory.java
@@ -41,4 +41,21 @@ public class numberTheory{
 
     return count;   
     }
+
+    //GCD of an Array
+    public int findGCD(int[] nums) {
+        int min = nums[0];
+        int max = nums[nums.length -1];
+        for(int i = 0;i < nums.length;i++){
+            if(nums[i]<min)
+            min = nums[i];
+            if(nums[i]>max)
+            max = nums[i];
+        }
+        return GCD(min,max);
+    }
+    private int GCD(int a,int b){
+        if(b == 0)return a;
+        return GCD(b,a % b);
+    }
 }
