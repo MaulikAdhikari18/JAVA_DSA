31fa9432faa620dbaa5d970aaa1361dfec2bf47d
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-02-09 09:34:04 +0530

    Convert Increasing Order BST

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class BSTNode{
     int val;
     BSTNode left;
@@ -16,6 +18,36 @@ class BSTNode{
         if(root.val > val)return searchBST(root.left,val);
         return searchBST(root.right,val);
     }
+
+    //Increasing order Search Tree
+    public BSTNode increasingBST(BSTNode root) {
+
+    List<Integer> list = new ArrayList<>();
+    inOrder(list, root);
+
+    BSTNode newRoot = null;
+    BSTNode curr = null;
+
+    for (int val : list) {
+        BSTNode node = new BSTNode(val);
+        if (newRoot == null) {
+            newRoot = node;
+            curr = node;
+        } else {
+            curr.right = node;
+            curr = node;
+        }
+    }
+
+    return newRoot;
+}
+
+private void inOrder(List<Integer> list, BSTNode root) {
+    if (root == null) return;
+    inOrder(list, root.left);
+    list.add(root.val);
+    inOrder(list, root.right);
+}
 }
 
 
