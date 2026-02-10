bb989e56ce24d7dd184430277349b239d88d769e
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-02-10 13:30:12 +0530

    Kth factor

--- src/numberTheory.java
+++ src/numberTheory.java
@@ -166,4 +166,15 @@ public class numberTheory{
         }
         return count;
     }
+
+    //Kth factor of n
+    public int kthFactor(int n, int k) {
+        if(k>n)return -1;
+        int count = 0;
+        for(int i = 1;i <= n;i++){
+            if(n%i == 0)count++;
+            if(count == k)return i;
+        }
+        return -1;
+    }
 }
