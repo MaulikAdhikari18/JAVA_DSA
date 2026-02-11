c880cf1e57efdb31492e44e49c0d72e59e5966cd
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-02-11 11:23:35 +0530

    Clear Bit & Update Bit

--- src/Learnings/BitManipulation.java
+++ src/Learnings/BitManipulation.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class BitManipulation {
 
-    public void getBit(int n,int pos){//AND
+    public static void getBit(int n,int pos){//AND
 
         int bitMask = 1<<pos;
         if((bitMask & n) == 0){
@@ -14,16 +14,44 @@ public class BitManipulation {
         }
     }
 
-    public void setBit(int n,int pos){//OR
+    public static void setBit(int n,int pos){//OR
         int bitMask = 1<<pos;
 
         int newNumber = bitMask | n;
-        System.out.println(newNumber);
+        System.out.println("Set Bit at position "+pos);
+        System.out.println("New Number: "+newNumber);
     }
+
+    public static void clearBit(int n,int pos){//AND with NOT
+        int bitMask = 1<<pos;
+
+        int newNumber = n & ~(bitMask);
+        System.out.println("Cleared Bit at position "+pos);
+        System.out.println("New Number: "+newNumber);
+    }
+
+    public static void updateBit(int n,int pos,int bit){
+        if(bit == 0){
+            System.out.print("Updated to 0: ");
+            clearBit(n, pos);
+            System.out.println("");
+        }
+        if(bit == 1){
+            System.out.print("Updated to 1: ");
+            setBit(n,pos);
+            System.out.println("");
+        }
+    }
+
     public static void main(String[] args) {
 
         int n = 5;
         int pos = 2;
-        
+
+        getBit(n,pos);
+        setBit(n, 1);
+        clearBit(n, pos);
+        updateBit(n, pos, 1);
+        updateBit(n, pos, 0);
     }    
 }
