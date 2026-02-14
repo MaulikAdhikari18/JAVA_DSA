524058fdddcad7751c876176380b449fc3d855fa
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-02-14 21:57:58 +0530

    Selection Sort

--- src/Learnings/Sorting.java
+++ src/Learnings/Sorting.java
@@ -1,7 +1,8 @@
 package Learnings;
 
 public class Sorting {
-    
+
+    //Bubble Sort
     public static void bubbleSort(int []nums){// O(n^2)
         for(int j = 0;j < nums.length-1;j++){
             for(int i = 0;i < nums.length-j-1;i++){
@@ -14,6 +15,20 @@ public class Sorting {
         }    
     }
 
+    //Selection Sort
+    public static void selectionSort(int []nums){
+        
+        for(int i = 0;i < nums.length-1;i++){
+            int min = i;
+            for(int j = i+1;j < nums.length;j++){
+                if(nums[j] < nums[min])min = j;
+            }
+            int temp = nums[min];
+            nums[min] = nums[i];
+            nums[i] = temp;
+        }
+    }
+
     public static void printArray(int []nums){
         for(int i = 0;i < nums.length;i++){
             System.out.print(nums[i]+" ");
@@ -24,7 +39,8 @@ public class Sorting {
         int arr[] = {23,12,62,46,34,98,54,29};
         System.out.print("Original Array:");
         printArray(arr);
-        bubbleSort(arr);
+        //bubbleSort(arr);
+        selectionSort(arr);
         System.out.print("\nSorted Array:");
         printArray(arr);
     }
