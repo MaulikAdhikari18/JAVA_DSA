8c2ac1030689435b0f477b38aaad65aee6b91e86
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-02-14 21:36:30 +0530

    Bubble Sort

---
+++ src/Learnings/Sorting.java
@@ -0,0 +1,31 @@
+package Learnings;
+
+public class Sorting {
+    
+    public static void bubbleSort(int []nums){// O(n^2)
+        for(int j = 0;j < nums.length-1;j++){
+            for(int i = 0;i < nums.length-j-1;i++){
+                if(nums[i+1] < nums[i]){
+                    int temp = nums[i];
+                    nums[i] = nums[i+1];
+                    nums[i+1] = temp;
+                }
+            }
+        }    
+    }
+
+    public static void printArray(int []nums){
+        for(int i = 0;i < nums.length;i++){
+            System.out.print(nums[i]+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {23,12,62,46,34,98,54,29};
+        System.out.print("Original Array:");
+        printArray(arr);
+        bubbleSort(arr);
+        System.out.print("\nSorted Array:");
+        printArray(arr);
+    }
+}
