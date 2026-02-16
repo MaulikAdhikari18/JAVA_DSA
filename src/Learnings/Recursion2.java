e6955d1b28b7846d0dae868c9b8384910a015cf3
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-02-16 11:56:15 +0530

    Tower of Hanoi

---
+++ src/Learnings/Recursion2.java
@@ -0,0 +1,21 @@
+package Learnings;
+
+public class Recursion2 {
+
+    //Tower Of Hanoi
+    //O(2^n  - 1)
+    public static void towerOfHanoi(int n,String src,String help,String dest){
+        if(n == 1){
+            System.out.println("Transfer disk "+n+" from "+src+" to "+dest);
+            return;
+        }
+        towerOfHanoi(n-1, src, dest, help);
+        System.out.println("Transfer disk "+n+" from "+src+" to "+dest);
+        towerOfHanoi(n-1, help, src, dest);
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        towerOfHanoi(n, "S", "H", "D");
+    }
+}
