76352f57b6f4e73520c8aab25691356d9d83db20
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-02-16 17:46:01 +0530

    Reverse String

--- src/Learnings/Recursion2.java
+++ src/Learnings/Recursion2.java
@@ -14,8 +14,30 @@ public class Recursion2 {
         towerOfHanoi(n-1, help, src, dest);
     }
 
+    //Print a String in reverse
+    public static void printReverseString(String s,int idx){
+        if(idx == 0){
+            System.out.println(s.charAt(idx));
+            return;
+        }
+        System.out.print(s.charAt(idx));
+        printReverseString(s, idx-1);
+    }
+
+    //First and last occurrence of a char
+    public static int first = -1;
+    public static int last = -1;
+    public static void firstLastOccur(String str,int idx,char element){
+        if(str.charAt(idx) == element){
+            if(first == -1)first = idx;
+            else last = idx;
+        }
+    }
+
     public static void main(String[] args) {
         int n = 3;
-        towerOfHanoi(n, "S", "H", "D");
+        //towerOfHanoi(n, "S", "H", "D");
+        String s = "Maulik";
+        printReverseString(s,s.length()-1);
     }
 }
