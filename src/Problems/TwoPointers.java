24d51f8ca5b3210e22b0462a1345cb7d7fac541a
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-02-17 11:51:25 +0530

    Move Zeros

--- src/Problems/TwoPointers.java
+++ src/Problems/TwoPointers.java
@@ -10,4 +10,18 @@ public class TwoPointers {
         }
         return false;
     }
+
+    //Move Zeros
+    public void moveZeroes(int[] nums) {
+        int pos = 0;
+
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] != 0){
+                int temp = nums[i];
+                nums[i] = nums[pos];
+                nums[pos] = temp;
+                pos++;
+            }
+        }
+    }
 }
