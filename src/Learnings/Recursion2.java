68efba52f0c540e377ea4bb5602807ada96d12b5
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-02-18 05:34:16 +0530

    Remove Duplicates

--- src/Learnings/Recursion2.java
+++ src/Learnings/Recursion2.java
@@ -61,14 +61,33 @@ public class Recursion2 {
         }
     }
 
+    //Remove Duplicates
+    public static boolean []map = new boolean[26];
+
+    public static void removeDuplicates(String str,int idx,String newString){
+        if(idx == str.length()){
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(map[currChar - 'a']){
+            removeDuplicates(str, idx+1, newString);
+        }else{
+            newString += currChar;
+            map[currChar - 'a'] = true;
+            removeDuplicates(str, idx+1, newString);
+        }
+    }
+
     public static void main(String[] args) {
-        int n = 3;
+        //int n = 3;
         //towerOfHanoi(n, "S", "H", "D");
         String s = "xaxbcdefgxhxi";
         //printReverseString(s,s.length()-1);
         // firstLastOccur(s, 0,'a' );
         // System.out.println("First index: "+first);
         // System.out.println("Last index: "+last);
-        moveX(s, 0, 0, "");
+        //moveX(s, 0, 0, "");
+        removeDuplicates(s, 0, "");
     }
 }
