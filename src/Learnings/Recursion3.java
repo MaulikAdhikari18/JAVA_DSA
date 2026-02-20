c6de0166927d67b9994638baf6a941ea642fc2cb
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-02-20 22:52:19 +0530

    Count Paths

--- src/Learnings/Recursion3.java
+++ src/Learnings/Recursion3.java
@@ -15,6 +15,19 @@ public class Recursion3 {
         }
     }
 
+    //Count Paths
+    public static int countPaths(int i,int j, int n,int m){
+        if(i == n || j == m)return 0;
+        if(i == n-1 && j == m-1)return 1;
+
+        //move downwards
+        int downPath = countPaths(i+1, j, n, m);
+
+        //move right
+        int rightPath = countPaths(i, j+1, downPath, m);
+        return downPath + rightPath;
+    }
+
     public static void main(String[] args) {
         String str = "abc";
         printPerm(str, "");
