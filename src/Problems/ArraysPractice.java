961028f20f05937e41fa26f2607b57aa0d2d2db8
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-02-22 21:27:49 +0530

    Running sum of Array

--- src/Problems/ArraysPractice.java
+++ src/Problems/ArraysPractice.java
@@ -91,4 +91,14 @@ public class ArraysPractice {
             nums1[k--] = nums2[j--];
         }
     }
+
+    //Running sum
+    public int[] runningSum(int[] nums) {
+        int []sum = new int[nums.length];
+        sum[0] = nums[0];
+        for(int i = 1;i < nums.length;i++){
+            sum[i] = sum[i-1] + nums[i];
+        }
+        return sum;
+    }
 }
