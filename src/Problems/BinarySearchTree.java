ce03109eb37a79a88dc163341e1cf4ae126e3141
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-02-25 05:50:30 +0530

    Valid BST

--- src/Problems/BinarySearchTree.java
+++ src/Problems/BinarySearchTree.java
@@ -57,6 +57,20 @@ class BSTNode{
         inOrder(list,root);
         return list.get(k-1);
     }
+
+    //Valid BST
+    long prev = Long.MIN_VALUE;
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) return true;
+
+        if (!isValidBST(root.left)) return false;
+
+        if (root.val <= prev) return false;
+        prev = root.val;
+
+        return isValidBST(root.right);
+    }
 }
 
 
