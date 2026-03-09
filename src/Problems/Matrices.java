c2f5bd7ec3bea23dbf0f08863fcfd3ad31a88ce3
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-03-09 21:35:34 +0530

    Reshape Matrix

--- src/Problems/Matrices.java
+++ src/Problems/Matrices.java
@@ -15,4 +15,18 @@ public class Matrices{
         }
         return result;
     }
+
+    //Reshape Matrix
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int m = mat.length, n = mat[0].length;
+
+    if (m * n != r * c) return mat;
+
+    int[][] result = new int[r][c];
+
+    for (int i = 0; i < m * n; i++) {
+        result[i / c][i % c] = mat[i / n][i % n];
+    }
+    return result;
+    }
 }
