b27748956ba40cfd6e98a6ea92bbcc5f2068bdf3
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-03-11 20:10:36 +0530

    Complement base 10

--- src/Problems/BitManipulation.java
+++ src/Problems/BitManipulation.java
@@ -10,4 +10,18 @@ public class BitManipulation {
         }
         return single;
     }
+
+    //Complement of Base 10
+    public int bitwiseComplement(int n) {
+        if(n == 0)return 1;
+
+        int mask = 0;
+        int temp = n;
+
+        while(temp > 0){
+            mask = (mask << 1) | 1;
+            temp >>= 1;
+        }
+        return n ^ mask;
+    }
 }
