c5bc3483c58a14aaf0ae40eab62d05fdb0bb7ba3
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-03-13 21:57:42 +0530

    Zigzag Conversion

--- src/Problems/Strings.java
+++ src/Problems/Strings.java
@@ -1,6 +1,8 @@
 package Problems;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Strings {
     public int romanToInt(String s) {
@@ -76,6 +78,37 @@ public class Strings {
                 result.append(symbols[i]);
             }
         }
+        return result.toString();
+    }
+
+    //Zigzag Conversion
+    public String convert(String s, int numRows) {
+        if(numRows == 1 || s.length() <= numRows)
+            return s;
+
+        List<StringBuilder> rows = new ArrayList<>();
+
+        for(int i = 0; i < numRows; i++)
+            rows.add(new StringBuilder());
+
+        int currentRow = 0;
+        boolean goingDown = false;
+
+        for(char c : s.toCharArray()) {
+
+            rows.get(currentRow).append(c);
+
+            if(currentRow == 0 || currentRow == numRows - 1)
+                goingDown = !goingDown;
+
+            currentRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for(StringBuilder row : rows)
+            result.append(row);
+
         return result.toString();
     }
 }
