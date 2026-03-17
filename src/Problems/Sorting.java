4eb2314cfa01e2cde84a2373c01b9c0329577310
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-03-17 14:04:36 +0530

    Insertion Sort

--- src/Problems/Sorting.java
+++ src/Problems/Sorting.java
@@ -14,4 +14,21 @@ public class Sorting{
         }
         return false;
     }
+
+    public static void insertionSort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];   // element to be inserted
+            int j = i - 1;
+
+            // Move elements greater than key one position ahead
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+    }
 }
