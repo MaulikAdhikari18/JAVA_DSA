8ce34b531a4cd6b3701e8350594606cb950e040f
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-03-18 21:09:05 +0530

    Permutation

--- src/Problems/Backtracking.java
+++ src/Problems/Backtracking.java
@@ -42,4 +42,33 @@ public class Backtracking {
             used[i] = false;
         }
     }
+
+    //46.Permutation
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+
+        backtrackp1(nums,used,new ArrayList<>(),result);
+        return result;
+    }
+
+    private void backtrackp1(int[] nums,boolean[] used,List<Integer> temp,List<List<Integer>> result){
+        if(temp.size() == nums.length){
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for(int i = 0;i < nums.length;i++){
+
+            if(used[i])continue;
+
+            used[i] = true;
+            temp.add(nums[i]);
+
+            backtrackp1(nums,used,temp,result);
+
+            temp.remove(temp.size()-1);
+            used[i] = false;
+        }
+    }
 }
