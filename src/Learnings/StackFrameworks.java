b11dd0bb1ad6fa838e09ea20fc2aa5fe0e675264
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-03-20 19:59:18 +0530

    Reverse Stack

--- src/Learnings/StackFrameworks.java
+++ src/Learnings/StackFrameworks.java
@@ -1,40 +1,33 @@
 package Learnings;
+
 import java.util.*;
 
 public class StackFrameworks {
-    static class Stack{
-        static ArrayList<Integer> list = new ArrayList<>();
 
-        //Is Empty
-        public static boolean isEmpty(){
-            return list.size() == 0;
+    public static void pushAtBottom(int data,Stack<Integer> s){
+        if(s.isEmpty()){
+            s.push(data);
+            return;
         }
-
-        //Push
-        public static void push(int data){
-            list.add(data);
-        }
-
-        //Pop
-        public static int pop(){
-            if(isEmpty())return -1;
-            int top = list.get(list.size()-1);
-            list.remove(list.size()-1);
-            return top;
-        }
-
-        //Peek
-        public static int peek(){
-            if(isEmpty())return -1;
-            return list.get(list.size()-1);
-        }
+        int top = s.pop();
+        pushAtBottom(data, s);
+        s.push(top);
     }
 
+    public static void reverse(Stack<Integer> s){
+        if(s.isEmpty())return;
+
+        int top = s.pop();
+        reverse(s);
+        pushAtBottom(top, s);
+    }
     public static void main(String[] args) {
-        Stack s = new Stack();
+        Stack<Integer> s = new Stack();
         s.push(1);
         s.push(2);
         s.push(3);
+
+        pushAtBottom(4, s);
 
         while(s.isEmpty()){
             System.out.println(s.peek());
