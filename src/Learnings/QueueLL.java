ad0bcdf16db486b348697236f922adcd6535b63e
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-03-21 15:15:06 +0530

    Queue using Linked List

---
+++ src/Learnings/QueueLL.java
@@ -0,0 +1,59 @@
+package Learnings;
+
+public class QueueLL {
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class Queue{
+        static Node head = null;
+        static Node tail = null;
+
+        public static boolean isEmpty(){
+            return head == null && tail == null;
+        }
+
+        //Enqueue
+        public static void add(int data){
+            Node newNode = new Node(data);
+            if(tail == null){
+                head = tail = newNode;
+                return;
+            }
+            tail.next = newNode;
+            tail = tail.next;
+        } 
+
+        //Dequeue
+        public static int remove(){
+            if(isEmpty()){
+                System.out.println("Queue Empty");
+                return -1;
+            }
+
+            int data = head.data;
+            if(head == tail){
+                head = tail = null;
+                return data;
+            }
+            head = head.next;
+            return data;
+        }
+
+        //Peek
+        public static int peek(){
+            if(isEmpty()){
+                System.out.println("Queue Empty");
+                return -1;
+            }
+
+            return head.data;
+        }
+    }
+}
