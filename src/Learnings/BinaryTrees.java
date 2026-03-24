4a9e167c407997c337a0d9a1e44f82b2a19a0246
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-03-24 13:15:43 +0530

    Height of Tree

--- src/Learnings/BinaryTrees.java
+++ src/Learnings/BinaryTrees.java
@@ -93,9 +93,18 @@ public class BinaryTrees {
         if(root == null)return 0;
         return root.data + sumOfNodes(root.left) + sumOfNodes(root.right);
     }
+
+    //Height of Tree
+    public static int height(Node root){
+        if(root == null)return 0;
+        int leftST = height(root.left);
+        int rightSt = height(root.right);
+
+        return 1 + Math.max(leftST,rightSt);
+    }
     
     public static void main(String[] args) {
-        int[] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        int[] nodes = {1,2,4,7,-1,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         //System.out.println(root.data);
@@ -108,5 +117,6 @@ public class BinaryTrees {
         //levelOrder(root);
         System.out.println(countNodes(root));
         System.out.println(sumOfNodes(root));
+        System.out.println("Height of Tree: "+height(root));
     }
 }
