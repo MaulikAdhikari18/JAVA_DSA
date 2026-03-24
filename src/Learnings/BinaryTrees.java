67538b2bcb5c03df41e0a401bb812bb3d7ce996f
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-03-24 17:19:00 +0530

    SubTree of a Tree

--- src/Learnings/BinaryTrees.java
+++ src/Learnings/BinaryTrees.java
@@ -144,6 +144,28 @@ public class BinaryTrees {
         return myInfo;
     }
 
+    //Identical Tree
+    public static boolean isIdentical(Node root,Node subRoot){
+        if(root == null && subRoot == null)return true;
+
+        if(root == null || subRoot == null)return false;
+
+        if(root.data == subRoot.data){
+            return isIdentical(root.left, subRoot.left) && isIdentical(root.right, subRoot.right);
+        }
+        return false;
+    }
+
+    //Subtree of a tree
+    public static boolean isSubtree(Node root,Node subRoot){
+        if(subRoot == null)return true;
+        if(root == null)return false;
+        if(root.data == subRoot.data){
+            if(isIdentical(root, subRoot))return true;
+        }
+
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
 
     public static void main(String[] args) {
         int[] nodes = {1,2,4,7,-1,-1,-1,5,-1,-1,3,-1,6,-1,-1};
