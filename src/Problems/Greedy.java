9c1e55fb372aa9ac70293e3e6141d6e1e60b182d
MaulikAdhikari18 <msadhikarintl123@gmail.com>
2026-04-09 11:00:00 +0530

    Valid Palindrome 2

--- src/Problems/Greedy.java
+++ src/Problems/Greedy.java
@@ -3,6 +3,8 @@ package Problems;
 import java.util.HashSet;
 
 public class Greedy {
+
+    //409. Longest Palindrome
     public int longestPalindrome(String s) {
         HashSet<Character> set = new HashSet<>();
         int length = 0;
@@ -22,5 +24,31 @@ public class Greedy {
         }
 
         return length;
+    }
+
+    //680. Valid Palindrome 2
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                // Try skipping either left or right character
+                return isPalindrome(s, left + 1, right) || 
+                       isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
     }
 }
